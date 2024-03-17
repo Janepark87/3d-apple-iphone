6c1064170fd39f694d5c2869375cb96d38bd1346
Jpark <web.jpark@gmail.com>
2024-03-16 20:12:30 -0600

    first init: setup, clean up files and add files

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+	const config = {
+		plugins: [react()],
+		base: command === 'serve' ? '/' : '/3d-iphone/',
+		assetsInclude: ['**/*.glb'],
+	};
+
+	return config;
+});
