e7e85d60e1e6163894711d5075279a3fa9ba9171
Jpark <web.jpark@gmail.com>
2024-03-27 23:08:35 -0600

    fixed the config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
 	const config = {
 		plugins: [react()],
-		base: command === 'serve' ? '/' : '/3d-iphone/',
+		base: command === 'serve' ? '/' : '/3d-apple-iphone/',
 		assetsInclude: ['**/*.glb'],
 	};
 
